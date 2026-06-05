85b7d23417d6056b7ee018c767dcc98055610452
Brahian Arias <aquelaronte@gmail.com>
2026-06-05 11:13:13 -0500

    chore: first version of initial migration

---
+++ main-server/db/migrations/20260605160252_init.tx.down.sql
@@ -0,0 +1,8 @@
+SET statement_timeout = 0;
+
+SELECT 1;
+
+DROP TABLE IF EXISTS item_variant;
+DROP TABLE IF EXISTS item;
+
+DROP TYPE IF EXISTS source;
---
+++ main-server/db/migrations/20260605160252_init.tx.up.sql
@@ -0,0 +1,33 @@
+SET statement_timeout = 0;
+
+SELECT 1;
+
+CREATE TYPE source AS ENUM ('clemont', 'undergold', 'own');
+
+CREATE TABLE item (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    price BIGINT NOT NULL,
+    source source NOT NULL DEFAULT 'own',
+    pictures TEXT[],
+
+    -- metadata
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    deleted_at TIMESTAMPTZ
+);
+
+CREATE TABLE item_variant (
+    id UUID,
+    name VARCHAR(255) NOT NULL,
+
+    item_id UUID NOT NULL REFERENCES item(id) ON DELETE CASCADE, -- reference to item
+
+    PRIMARY KEY(item_id, id),
+
+    -- metadata
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    deleted_at TIMESTAMPTZ
+);
